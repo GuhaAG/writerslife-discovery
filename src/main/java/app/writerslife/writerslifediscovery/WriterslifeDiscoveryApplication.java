6ff8187f60ffc7d430ba20e78ae3b3705256c004
guhaAG <guha.agt@gmail.com>
2019-10-26 21:15:04 +0900

    init eureka discovery client

---
+++ src/main/java/app/writerslife/writerslifediscovery/WriterslifeDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package app.writerslife.writerslifediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class WriterslifeDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriterslifeDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/writerslife/writerslifediscovery/WriterslifeDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package app.writerslife.writerslifediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriterslifeDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
