4dda998730651bfd083201f711fd052f1ff80233
guhaAG <guha.agt@gmail.com>
2019-12-07 01:49:12 +0900

    Refactor class names to be more meaningful

--- src/main/java/app/writerslife/writerslifediscovery/DiscoveryApplication.java
+++ src/main/java/app/writerslife/writerslifediscovery/DiscoveryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class WriterslifeDiscoveryApplication {
+public class DiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WriterslifeDiscoveryApplication.class, args);
+		SpringApplication.run(DiscoveryApplication.class, args);
 	}
 
 }
